5c49ab631517aa2e11416ff32d89d7a28505a8be
Andrew <abradley201@gmail.com>
2015-11-07 13:12:28 -0500

    database adjustments

--- app/controllers/board1_controller.rb
+++ app/controllers/board1_controller.rb
@@ -2,8 +2,8 @@ class Board1Controller < ApplicationController
 	def go1
 
     #find out who SelectedHero is
-    #give positions to those who have pos = 0
-    #assign BoardCode
+    #give positions when pos == 0, based on allies being white/black
+    #create Boardcode for map_stats
 
     
 	end
