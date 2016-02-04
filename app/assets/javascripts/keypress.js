8deda8088db92c89e5d14eec13c45d489a09df71
Andrew <abradley201@gmail.com>
2016-02-03 22:15:23 -0500

    Asdads

--- app/assets/javascripts/keypress.js
+++ app/assets/javascripts/keypress.js
@@ -52,7 +52,7 @@ function google(evt){
 
     									 }
 
-    if (String.fromCharCode(key)==" "){ 
+    if (String.fromCharCode(key)=="q"){ 
 
                                         command = "1";
 
