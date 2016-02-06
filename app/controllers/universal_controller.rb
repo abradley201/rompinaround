815358c3db67e8a1e3e8dc03649b4780e23a751b
Andrew <abradley201@gmail.com>
2016-02-06 07:52:07 -0500

    asdad

--- app/controllers/universal_controller.rb
+++ app/controllers/universal_controller.rb
@@ -310,11 +310,11 @@ class UniversalController < ApplicationController
 
 						@whiteCoreHP = 0
 
-						@winningAccount = GameStat.where(:game => @gameNumber, :allies => "black")
+						@winningAccount = GameStat.where(:game => @gameNumber, :allies => "black").account
 
 						@CW = InfoStat.find_by_account(@winningAccount).wins
 
-						@losingAccount = GameStat.where(:game => @gameNumber, :allies => "white")
+						@losingAccount = GameStat.where(:game => @gameNumber, :allies => "white").account
 
 						@CL = InfoStat.find_by_account(@losingAccount).losses
 
@@ -342,11 +342,11 @@ class UniversalController < ApplicationController
 
 						@blackCoreHP = 0
 
-						@winningAccount = GameStat.where(:game => @gameNumber, :allies => "white")
+						@winningAccount = GameStat.where(:game => @gameNumber, :allies => "white").account
 
 						@CW = InfoStat.find_by_account(@winningAccount).wins
 
-						@losingAccount = GameStat.where(:game => @gameNumber, :allies => "black")
+						@losingAccount = GameStat.where(:game => @gameNumber, :allies => "black").account
 
 						@CL = InfoStat.find_by_account(@losingAccount).losses
 
